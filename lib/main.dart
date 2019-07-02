b40b40d4e18367b6e5cf9fe54161111221016c6b
bobo <bobo@email.com>
2019-07-02 11:12:26 +0800

    ADD: text stype

--- lib/main.dart
+++ lib/main.dart
@@ -143,10 +143,42 @@ class NewRoute extends StatelessWidget {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("New page"),
+        title: Text("New page", textAlign: TextAlign.center,),
       ),
       body: new Container(
-        decoration: new BoxDecoration(image: new DecorationImage(colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.6), BlendMode.dstATop),image: new AssetImage("assets/images/ucenter_image_six.jpg"),fit: BoxFit.cover,)),
+        decoration: new BoxDecoration(image: new DecorationImage(
+            colorFilter: new ColorFilter.mode(
+                Colors.black.withOpacity(0.6), BlendMode.dstATop),
+            image: new AssetImage("assets/images/ucenter_image_six.jpg"),
+            fit: BoxFit.cover)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("normal",textAlign: TextAlign.left,),
+            Text("normal text for lines" *4,maxLines: 1,overflow: TextOverflow.ellipsis,textAlign: TextAlign.left,),
+            Text("normal text for big ",textScaleFactor: 1.5,textAlign: TextAlign.left,),
+            Text("text for style",
+            style:  TextStyle(
+              color: Colors.red,
+              fontSize: 18.0,
+              height: 1.4,
+              fontFamily: "Courier",
+              background: new Paint()..color= Colors.yellow,
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.dashed,
+            ),
+            ),
+            Text.rich(TextSpan(
+              children: [TextSpan(text: "Home:"),
+                TextSpan(text: "https://flutterchina.club",style: TextStyle(color: Colors.blue),),
+              ]
+
+            ))
+
+          ],
+        ),
+        width: double.infinity,
+        height: double.infinity,
       ),
 
     );
